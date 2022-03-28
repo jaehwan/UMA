b9c4906ba4cdcfde70c7f1ae5e2adc2efec85d41
keepon <keepon1@gmail.com>
2022-03-28 21:38:52 +0900

    리셋

---
+++ Assets/Scripts/UMATest.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+// using UMA.CharacterSystem;
+using UnityEngine;
+
+public class UMATest : MonoBehaviour
+{
+
+    GameObject newUMA;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // newUMA = new GameObject();
+        // newUMA.AddComponent<DynamicCharacterAvatar>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
